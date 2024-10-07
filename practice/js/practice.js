dd42d1c1632540d6f9bd71adda4828a73bfd76bc
Awais Waheed <awaiswaheed321@gmail.com>
2024-10-07 13:44:08 -0500

    Practice Async

---
+++ practice/js/practice.js
@@ -0,0 +1,95 @@
+// // console.log("start");
+
+// const func = (name) => {
+//   console.log(`${name} Hi`);
+// };
+
+// // const timerId = setTimeout(func, 1000, "JOhn");
+// // setTimeout(func, 1000, "Mari");
+// // setTimeout(func, 2000, "Doe");
+// setTimeout(func, 1000, "Pen");
+// // clearTimeout(timerId);
+// // console.log("End");
+
+// let timerId = setInterval(() => console.log('tick'), 1000);
+// // after 5 seconds stop
+// setTimeout(() => { clearInterval(timerId); console.log('stop'); }, 5000);
+
+// const giveMePizza = () =>
+//   new Promise((resolve, reject) => {
+//     if (false) {
+//       resolve("Give Pizza");
+//     } else {
+//       reject("No Pizza for you");
+//     }
+//   });
+
+// giveMePizza()
+//   .then((data) => console.log(data))
+//   .catch((err) => console.error(err));
+
+// console.log("Run this before");
+
+// const promise = new Promise((resolve, reject) => {
+//   console.log(`Promise starts`);
+//   resolve(`Promise result`);
+//   console.log(`Promise ends`);
+// });
+// console.log(`Code starts`);
+// promise.then(console.log);
+// console.log(`Code ends`);
+
+// const promise = new Promise((resolve, reject) => {
+//   reject("error");
+//   setTimeout(() => {
+//     resolve("Promise results");
+//   }, 2000); // resolve after 1 second
+// });
+// console.log("Code starts");
+// promise
+//   .then(console.log)
+//   .catch(console.log)
+//   .finally(() => console.log("finally"));
+// console.log("I love JS");
+
+// let promise = new Promise(function (resolve, reject) {
+//   const random = Math.random();
+//   console.log(" random: ", random);
+//   if (random > 0.5) setTimeout(() => resolve("done"), 1000);
+//   else setTimeout(() => reject(new Error("Woops!")), 1000);
+// });
+// promise
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error))
+//   .finally(() => console.log("Promise ready!"));
+
+// const p = new Promise(function (resolve, reject) {
+//   setTimeout(() => resolve(1), 1000); // (*)
+// })
+//   .then(function (result) {
+//     console.log(result); // 1
+//     return result * 2;
+//   })
+//   .then(function (result) {
+//     console.log(result); // 2
+//     return result * 2;
+//   })
+//   .then(function (result) {
+//     console.log(result); // 4
+//     return result * 2;
+//   });
+
+// p.then((r) => console.log(r)).catch((e) => console.log(e));
+
+console.log("start");
+async function foo() {
+  return " done ! ";
+}
+async function bar() {
+  console.log(" insidebar - start");
+  let result = await foo();
+  console.log(result);
+  console.log("insidebar - end");
+}
+bar();
+console.log("end");
