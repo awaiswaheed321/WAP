857b480e534b6f69de19a5c3ac60567f01f08799
Awais Waheed <awaiswaheed321@gmail.com>
2024-10-07 14:13:22 -0500

    Lab7 Q1 updated

--- lab7/Q1.js
+++ lab7/Q1.js
@@ -1,28 +1,14 @@
 question1();
 
 function question1() {
-  const promise = new Promise((resolve, reject) => {
-    console.log("Start Meditation");
-    resolve(5);
-  })
-    .then((res) => {
-      console.log(res);
-      return res - 1;
-    })
-    .then((res) => {
-      console.log(res);
-      return res - 1;
-    })
-    .then((res) => {
-      console.log(res);
-      return res - 1;
-    })
-    .then((res) => {
-      console.log(res);
-      return res - 1;
-    })
-    .then((res) => {
-      console.log(res);
-    })
-    .finally(() => console.log("Meditation Started"));
+  console.log("Start Meditation...");
+  let timer = 5;
+  const interval = setInterval(() => {
+    console.log(timer--);
+  }, 1000);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    console.log("Meditation Started...");
+  }, 6000);
 }
