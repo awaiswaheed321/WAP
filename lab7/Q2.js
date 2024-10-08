8a5cf2c2e6aea09fb53abfe50422c5610f0c1c89
Awais Waheed <awaiswaheed321@gmail.com>
2024-10-08 10:02:35 -0500

    Q2 Fixes

--- lab7/Q2.js
+++ lab7/Q2.js
@@ -1,33 +1,33 @@
-question2();
-
-function question2() {
+async function question2() {
   console.log("start");
   for (let i = 0; i < 10; i++) {
     const n = getRandomNaturalNumber();
-    isPrime(n)
-      .then((res) => {
-        console.log(`${n} is: ${JSON.stringify(res)}`);
-      })
-      .catch((err) => {
-        console.error(`${n} is: ${JSON.stringify(err)}`);
-      });
+    try {
+      const res = await isPrime(n);
+      console.log(`${n} is: ${JSON.stringify(res)}`);
+    } catch (err) {
+      console.error(`${n} is: ${JSON.stringify(err)}`);
+    }
   }
   console.log("end");
 }
 
-function isPrime(n) {
+async function isPrime(n) {
   return new Promise((resolve, reject) => {
+    if (n <= 1) {
+      return reject({ prime: false });
+    }
     for (let i = 2, s = Math.sqrt(n); i <= s; i++) {
-      if (n % i === 0) reject({ prime: false });
+      if (n % i === 0) {
+        return resolve({ prime: false });
+      }
     }
-    if (n > 1) {
-      resolve({ prime: true });
-    } else {
-      reject({ prime: false });
-    }
+    resolve({ prime: true });
   });
 }
 
 function getRandomNaturalNumber() {
   return Math.floor(Math.random() * 1000) + 1;
 }
+
+question2();
