5425c7513b279d978da25e26d284badeb6bfb664
Awais Waheed <awaiswaheed321@gmail.com>
2024-10-16 18:00:41 -0500

    Students Component Base Code Added

--- lab12-13/src/App.js
+++ lab12-13/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import AddStudent from "./AddStudent.jsx";
+import Students from "./Students.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AddStudent />
+      <Students />
       <Footer />
     </div>
   );
---
+++ lab12-13/src/Students.jsx
@@ -0,0 +1,14 @@
+export default function Students() {
+  return (
+    <div className="container">
+      <div className="students-container">
+        <h1>All Students</h1>
+        <table className="students-table">
+          <th>ID</th>
+          <th>Name</th>
+          <th>Program</th>
+        </table>
+      </div>
+    </div>
+  );
+}
