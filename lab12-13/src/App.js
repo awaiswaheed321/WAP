861de0efb0e8dd7eca095e9f88c12e630ee3e3b6
Awais Waheed <awaiswaheed321@gmail.com>
2024-10-16 17:33:28 -0500

    Code Added for 3 components

---
+++ lab12-13/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import AddStudent from "./AddStudent.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AddStudent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ lab12-13/src/Footer.jsx
@@ -0,0 +1,6 @@
+export default function Footer() {
+  return <div className="footer">
+    <span className="footer-left">:::CS472-WAP</span>
+    <span className="footer-right">&copy; October 2024</span>
+  </div>;
+}
