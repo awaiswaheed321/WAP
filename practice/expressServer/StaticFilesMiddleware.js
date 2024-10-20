3b3d4ecfcea492a58116ee86a71aecd418c9d850
Awais Waheed <awaiswaheed321@gmail.com>
2024-10-20 11:17:04 -0500

    Static Middleware

---
+++ practice/expressServer/StaticFilesMiddleware.js
@@ -0,0 +1,5 @@
+const StaticFilesMiddleware = (req, res, next) => {
+    
+}
+
+export default StaticFilesMiddleware;
--- practice/expressServer/app.js
+++ practice/expressServer/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import ExpenseRouter from "./ExpenseRouter.js";
 import ErrorMiddleware from "./ErrorMiddleware.js";
 import LoggingMiddleware from "./LoggingMiddleware.js";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 app.set("port", 3000);
@@ -9,6 +11,10 @@ const port = app.get("port");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(LoggingMiddleware);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use("/api/v1/files", express.static(join(__dirname, "data")));
 app.use("/api/v1/expense", ExpenseRouter);
 app.use(ErrorMiddleware);
 app.listen(port, () => {
